11827ca82dc4e3ee9c9759f133c857f2ac6aa4f0
Fadi Asaad <firon1222@gmail.com>
2023-02-16 18:23:47 +0200

    hotfix `4-puts.c`: add docs to my program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - simple main function that prints msg to stdout
+ * Return: 0 exit code
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
