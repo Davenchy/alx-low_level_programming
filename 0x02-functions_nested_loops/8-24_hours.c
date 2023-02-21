95ab7078134ddca065d8c56c26641441d998ffeb
Fadi Asaad <firon1222@gmail.com>
2023-02-21 18:46:31 +0200

    add source file `8-24_hours.c` with function `jack_bauer`

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+void print_int(int n)
+{
+  int a, b;
+
+  b = n % 10;
+  a = n / 10;
+
+  _putchar(a + 48);
+  _putchar(b + 48);
+}
+
+/**
+ * jack_bauer - print every minute of the day
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int i;
+
+	for (i = 0; i < 1440; i++)
+	{
+		int m, h;
+		
+		h = i / 60;
+		m = i % 60;
+
+    print_int(h);
+    _putchar(':');
+    print_int(m);
+    _putchar('\n');
+	}
+}
