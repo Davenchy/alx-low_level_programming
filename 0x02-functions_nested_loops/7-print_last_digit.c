2d7e35c270b39d10592aa2df5995aa23182dd8b2
Fadi Asaad <firon1222@gmail.com>
2023-02-21 15:06:22 +0200

    fix `print_last_digit`: print negative numbers

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_last_digit - print the last digit of a number
@@ -9,10 +10,10 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	if (n < 0)
-		n *= -1;
-
 	last_digit = n % 10;
+	if (last_digit < 0)
+		last_digit *= -1;
+
 	_putchar(last_digit + '0');
 	return (last_digit);
 }
