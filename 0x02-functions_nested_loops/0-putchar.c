400f576cef1489a1f8540aebe5f28f0ff60d629c
Fadi Asaad <firon1222@gmail.com>
2023-02-21 12:40:52 +0200

    fix linting errors for `0-putchar.c`

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,18 @@
 #include "main.h"
 
-int main(void) {
+/**
+ * main - prints "_putchar"
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
 	int i;
 	char *msg = "_putchar";
 
 	for (i = 0; i < 8; i++)
 		_putchar(msg[i]);
-	
+
 	_putchar('\n');
 	return (0);
 }
