2689d7a0b80cc148811ce12a392983afd969599c
Fadi Asaad <firon1222@gmail.com>
2023-02-23 18:03:39 +0200

    add task `5-more_numbers`

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - print from 0 to 14 in one line x10
+ * Return: void
+ */
+void more_numbers(void)
+{
+	char i, j, a, b;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			a = j / 10;
+			b = j % 10;
+
+			if (j >= 10)
+				_putchar(a + '0');
+			_putchar(b + '0');
+		}
+		_putchar('\n');
+	}
+}
