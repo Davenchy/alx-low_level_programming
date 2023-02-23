cdee40d54ddbb85fa079fa9daa4f877211bf7ea5
Fadi Asaad <firon1222@gmail.com>
2023-02-23 19:02:17 +0200

    add task `10-triangles`

---
+++ 0x04-more_functions_nested_loops/10-triangles.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar(j > size - i - 2 ? '#' : ' ');
+		}
+		_putchar('\n');
+	}
+}
