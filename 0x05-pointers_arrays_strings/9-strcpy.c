7a3d44e35b3fdb206f3058c47934374b54a0c850
Fadi Asaad <firon1222@gmail.com>
2023-02-27 21:25:03 +0200

    add task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+/**
+ *
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	char c;
+
+	i = 0;
+	while (1)
+	{
+		c = *(src + i);
+		*(dest + i++) = c;
+		if (c == '\0')
+			break;
+	}
+	return (dest);
+}
