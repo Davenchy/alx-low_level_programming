39e4922d6544394ccf86c930e502d823f1362733
Fadi Asaad <firon1222@gmail.com>
2023-03-01 14:37:41 +0200

    add comments to `0-strcat.c`

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,10 +8,13 @@ char *_strcat(char *dest, char *src)
 {
 	char *p = dest;
 
+	/* move dest pointer to the end of the string */
 	for (; *dest; dest++)
 		{}
+	/* copy from src to dest */
 	for (; *src; src++, dest++)
 		*dest = *src;
+	/* set null char at the new end */
 	*dest = 0;
 	return (p);
 }
