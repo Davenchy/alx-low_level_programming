ddfa9de1a1a06153bcbdfc8679deb1dc22e887a2
Fadi Asaad <firon1222@gmail.com>
2023-03-02 13:10:18 +0200

    fix task2: `2-main.c`, `2-strncpy.c`

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,11 +12,16 @@ char *_strncpy(char *dest, char *src, int n)
 	char *p = dest;
 
 	/* copy from src to dest */
-	for (; *dest && *src && n > 0; src++, dest++, n--)
+	for (; *src && n > 0; src++, dest++, n--)
 		*dest = *src;
 
+	/* set new end of string */
 	if (!*src)
 		*dest = *src;
+
+	/* write null for remaining bytes if exists */
+	for (; n > 0; n--, dest++)
+		*dest = 0;
 
 	return (p);
 }
