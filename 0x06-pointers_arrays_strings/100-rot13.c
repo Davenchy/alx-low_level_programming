c5869ceec6ebe9cd6d9f594016724c5210c67f07
Fadi Asaad <firon1222@gmail.com>
2023-03-02 17:23:44 +0200

    add task100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,32 @@
+/**
+ * rot13_in_range - encode char @c using rot13 using characters range @a to @z
+ * @a: the start of alphabit
+ * @z: the end of alphabit
+ * @c: the reference to the character to encode using rot13
+ * Return: 0 if @c is not in the range @a to @z otherwise 1
+ */
+int rot13_in_range(char a, char z, char *c)
+{
+	if (*c >= a && *c <= z)
+	{
+		*c -= a;
+		*c = ((*c + 13) % 26) + a;
+		return 1;
+	}
+	return 0;
+}
+
+/**
+ * rot13 - encode string using rot13
+ * @str: the reference to string to encode
+ * Return @str
+ */
+char *rot13(char *str)
+{
+	int i;
+	
+	for (i = 0; *(str + i); i++)
+		if (!rot13_in_range('a', 'z', str + i))
+			rot13_in_range('A', 'Z', str + i);
+	return str;
+}
