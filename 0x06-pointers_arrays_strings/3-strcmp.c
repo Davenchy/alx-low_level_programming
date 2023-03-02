12ea0e90ab85ae3891f6154f8c2401bcb6b2ce20
Fadi Asaad <firon1222@gmail.com>
2023-03-02 12:41:30 +0200

    add task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+/**
+ * _strcmp - compare @s1 and @s2
+ * @s1: the 1st number to compare
+ * @s2: the 2nd number to compare
+ * Return: 0 if @s1 == @s2
+ * positive integer if @s1 > @s2
+ * negaitve integer if @s1 < @s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (*s1 == *s2 ? 0 : *s1 > *s2 ? 15 : -15);
+}
