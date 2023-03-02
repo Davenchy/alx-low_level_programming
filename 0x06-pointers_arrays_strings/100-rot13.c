825901df4112a92991a4e72ef804c6df05816fc3
Fadi Asaad <firon1222@gmail.com>
2023-03-02 17:53:09 +0200

    update `100-rot13.c`

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -3,17 +3,14 @@
  * @a: the start of alphabit
  * @z: the end of alphabit
  * @c: the reference to the character to encode using rot13
- * Return: 0 if @c is not in the range @a to @z otherwise 1
  */
-int rot13_in_range(char a, char z, char *c)
+void rot13_in_range(char a, char z, char *c)
 {
 	if (*c >= a && *c <= z)
 	{
 		*c -= a;
 		*c = ((*c + 13) % 26) + a;
-		return (1);
 	}
-	return (0);
 }
 
 /**
@@ -26,7 +23,9 @@ char *rot13(char *str)
 	int i;
 
 	for (i = 0; *(str + i); i++)
-		if (!rot13_in_range('a', 'z', str + i))
-			rot13_in_range('A', 'Z', str + i);
+	{
+		rot13_in_range('a', 'z', str + i);
+		rot13_in_range('A', 'Z', str + i);
+	}
 	return (str);
 }
