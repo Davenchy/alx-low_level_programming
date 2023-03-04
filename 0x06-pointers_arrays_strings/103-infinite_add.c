9fad960dd5d809526ebee8dd86529a8c5c0d3cf3
Fadi Asaad <firon1222@gmail.com>
2023-03-04 13:01:52 +0200

    update task 103 - `103-infinite_add.c`

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,78 +1,69 @@
-#include "main.h"
+#include <stdio.h>
+#include <string.h>
 
 /**
- * rev_string - reverse array
- * @n: integer params
- * Return: 0
+ * strshiftl - shift string to left @n characters
+ * @str: the string reference
+ * @size: the size of selected string buffer to shift
+ * @n: the number of left shifts
+ * Description:
+ * shift @size characters of @str @n times to left
+ * with null characters right padding
+ * and a null character at the end (index: @size + @steps)
  */
-
-void rev_string(char *n)
+void strshiftl(char *str, int size, int n)
 {
-	int i = 0;
-	int j = 0;
-	char temp;
 
-	while (*(n + i) != '\0')
-	{
-		i++;
-	}
-	i--;
+	*(str + size + n) = 0;
+	for (; size > 0; size--)
+		*(str + size + n - 1) = *(str + size - 1);
 
-	for (j = 0; j < i; j++, i--)
-	{
-		temp = *(n + j);
-		*(n + j) = *(n + i);
-		*(n + i) = temp;
-	}
+	for (; n > 0; n--)
+		*(str + n - 1) = 0;
 }
 
 /**
- * infinite_add - add 2 numbers together
- * @n1: text representation of 1st number to add
- * @n2: text representation of 2nd number to add
- * @r: pointer to buffer
- * @size_r: buffer size
- * Return: pointer to calling function
+ * infinite_add - sum 2 strings as numbers
+ * @n1: the reference to 1st string
+ * @n2: the reference to 2nd string
+ * @r: the reference to the result string
+ * @size_r: the allocated size for the result string (@r)
+ * Return: a pointer to the result string (@r) or Null on fail
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int overflow = 0, i = 0, j = 0, digits = 0;
-	int val1 = 0, val2 = 0, temp_tot = 0;
+	int i, asize, bsize, msize;
+	char c1, c2, cu = 0;
 
-	while (*(n1 + i) != '\0')
-		i++;
-	while (*(n2 + j) != '\0')
-		j++;
-	i--;
-	j--;
-	if (j >= size_r || i >= size_r)
-		return (0);
-	while (j >= 0 || i >= 0 || overflow == 1)
+	asize = strlen(n1);
+	bsize = strlen(n2);
+	msize = asize > bsize ? asize : bsize;
+	if (msize > size_r - 1)
+		return (NULL);
+
+	for (i = 1; i <= msize; i++)
 	{
-		if (i < 0)
-			val1 = 0;
-		else
-			val1 = *(n1 + i) - '0';
-		if (j < 0)
-			val2 = 0;
-		else
-			val2 = *(n2 + j) - '0';
-		temp_tot = val1 + val2 + overflow;
-		if (temp_tot >= 10)
-			overflow = 1;
-		else
-			overflow = 0;
-		if (digits >= (size_r - 1))
-			return (0);
-		*(r + digits) = (temp_tot % 10) + '0';
-		digits++;
-		j--;
-		i--;
+		int over = 0;
+
+		c1 = i > asize ? 0 : *(n1 + asize - i) - '0';
+		c2 = i > bsize ? 0 : *(n2 + bsize - i) - '0';
+		c1 += c2 + cu;
+		over = c1 >= 10;
+		cu = over ? c1 / 10 : 0;
+		if (over)
+			c1 %= 10;
+
+		*(r + msize - i) = c1 + '0';
 	}
-	if (digits == size_r)
-		return (0);
-	*(r + digits) = '\0';
-	rev_string(r);
+
+	if (cu && size_r - 1 <= msize)
+		return (NULL);
+
+	if (cu && size_r - 1 > msize)
+	{
+		strshiftl(r, msize, 1);
+		*r = cu + '0';
+	}
+
 	return (r);
 }
