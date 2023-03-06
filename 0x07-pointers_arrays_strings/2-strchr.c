86a994cbf393a8ee4fbd11b9d5b2d50dd0a07bc6
Fadi Asaad <firon1222@gmail.com>
2023-03-06 17:25:32 +0200

    add description to task 2: `2-strchr.c`

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,3 +1,9 @@
+/**
+ * _strchr - locates a character @c in a string @s
+ * @s: the reference to string
+ * @c: the character to locate in the string
+ * Return: Null if not found otherwise the pointer to @c
+ */
 char *_strchr(char *s, char c)
 {
 	for (; *s && *s != c; s++)
