ad2db9114725b7601425aa00bf381c15d1691fcc
Fadi Asaad <firon1222@gmail.com>
2023-03-07 19:38:10 +0200

    update task 100

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * set_string - sets pointer val to char
- * @s: pointer
- * @to: char
+ * set_string - resets @s to @to
+ * @s: the reference to a pointer that will be modified
+ * @to: the char reference to be set to @s
  */
 void set_string(char **s, char *to)
 {
