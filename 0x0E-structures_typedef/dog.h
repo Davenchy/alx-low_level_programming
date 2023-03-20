8717710f8dbc33b8f3032c87aec0d6fc0ff7df7d
Fadi Asaad <firon1222@gmail.com>
2023-03-20 16:40:28 +0200

    add task 3: define type `dog_t`

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
  * struct dog - dog data structure
  * @name: the dog's name (cstring)
  * @owner: the dog's owner name (cstring)
  * @age: the dog's age
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	char *owner;
 	float age;
-};
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
