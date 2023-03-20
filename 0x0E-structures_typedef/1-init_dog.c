cb4a87579d79d953dde8378ef57186b99cf75237
Fadi Asaad <firon1222@gmail.com>
2023-03-20 15:04:30 +0200

    add task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize struct dog
+ * @d: reference to struct dog
+ * @name: the dog's name (cstring)
+ * @age: the dog's age
+ * @owner: the dog's owner name (cstring)
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->owner = owner;
+	d->age = age;
+}
