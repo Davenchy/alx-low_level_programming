869ab878d6e7ee525ba83dde861dea4e1fb42ca0
Fadi Asaad <firon1222@gmail.com>
2023-03-22 18:33:57 +0200

    add task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,9 @@
+/**
+ * print_name - prints a name using util function @f
+ * @name: the name characters to print
+ * @f: the util function to print the name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
