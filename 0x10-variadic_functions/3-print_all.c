ce4161eba203ec1143b190a6a75978a5d3bfc4ed
Fadi Asaad <firon1222@gmail.com>
2023-03-23 22:27:04 +0200

    update task 3: use switch case solution

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,80 +3,44 @@
 #include "variadic_functions.h"
 
 /**
- * print_string - format function for string case
- * @ap: pointer to the variadic list
- * @suffix: string to append at the end of the output
- */
-void print_string(va_list ap, char *suffix)
-{
-	printf("%s%s", suffix,  va_arg(ap, char *));
-}
-
-/**
- * print_integer - format function for integer case
- * @ap: pointer to the variadic list
- * @suffix: string to append at the end of the output
- */
-void print_integer(va_list ap, char *suffix)
-{
-	printf("%s%d", suffix,  va_arg(ap, int));
-}
-
-/**
- * print_float - format function for float case
- * @ap: pointer to the variadic list
- * @suffix: string to append at the end of the output
- */
-void print_float(va_list ap, char *suffix)
-{
-	printf("%s%f", suffix,  va_arg(ap, double));
-}
-
-/**
- * print_character - format function for character case
- * @ap: pointer to the variadic list
- * @suffix: string to append at the end of the output
- */
-void print_character(va_list ap, char *suffix)
-{
-	printf("%s%c", suffix,  va_arg(ap, int));
-}
-
-/**
  * print_all - prints anything as described in @format
  * @format: a string that describes the types of arguments
  */
 void print_all(const char * const format, ...)
 {
-	int i = 0, j;
-	char *sep = "";
-	char *suffix = ", ";
+	int i = 0;
+	char *sep = "", *str;
 	va_list ap;
-	pformat formats[] = {
-		{ 'c', print_character },
-		{ 's', print_string },
-		{ 'i', print_integer },
-		{ 'f', print_float },
-		{ 0, NULL }
-	};
 
 	if (format)
 	{
 		va_start(ap, format);
 		while (format[i])
 		{
-			j = 0;
-			while (formats[j].key)
+			switch (format[i++])
 			{
-				if (formats[j].key == format[i])
-				{
-					formats[j].fun(ap, sep);
-					sep = suffix;
+				case 'c':
+					printf("%s%c", sep, va_arg(ap, int));
+					sep = ", ";
 					break;
-				}
-				j++;
+				case 's':
+					str = va_arg(ap, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
+					sep = ", ";
+					break;
+				case 'i':
+					printf("%s%i", sep, va_arg(ap, int));
+					sep = ", ";
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(ap, double));
+					sep = ", ";
+					break;
+				default:
+					break;
 			}
-			i++;
 		}
 		va_end(ap);
 	}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,16 +7,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-typedef void (*pfun)(va_list, char *);
-/**
- * struct pformat - a struct to hold format case and its function
- * @key: char represents the format case
- * @fun: a function that prints the format case
- */
-typedef struct pformat
-{
-	char key;
-	pfun fun;
-} pformat;
-
 #endif
