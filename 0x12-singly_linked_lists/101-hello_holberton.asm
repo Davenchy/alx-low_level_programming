7208eb53bffd233b484057a11a35bfc7848fad1b
Fadi Asaad <firon1222@gmail.com>
2023-03-31 01:59:29 +0200

    update task 101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,11 @@
-global _start
+global main
+main:
+mov rdi, msg
+mov rsi, 0
+mov al, 0
 
-section .text
+extern printf
+call printf
 
-_start:
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, msg
-	mov rdx, msglen
-	syscall
-
-	mov rax, 60
-	mov rdi, 0
-	syscall
-
-section .rodata
-	msg: db "Hello Holberton", 10
-	msglen: equ $ - msg
+ret
+msg: db "Hello, Holberton", 10, 0
