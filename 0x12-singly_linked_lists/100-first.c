e5eba6fd01cb8e01777e1851cd408d4448c111f7
Fadi Asaad <firon1222@gmail.com>
2023-03-31 02:06:25 +0200

    add task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/**
+ * premain - a function to execute before the main function
+ */
+void __attribute__((constructor)) premain(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
