471b96906ca32c16bf9cab42914b29ac7821e5bd
Fadi Asaad <firon1222@gmail.com>
2023-04-07 20:14:10 +0200

    update tasks 2, 3, 4

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,7 +7,7 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (!n || index >= sizeof(long int) * 8)
+	if (!n || index >= sizeof(*n) * 8)
 		return (-1);
 	*n |= 1UL << index;
 	return (1);
