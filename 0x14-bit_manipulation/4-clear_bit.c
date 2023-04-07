481d0608ed1ed00604324dc34467fc7a28e2ec88
Fadi Asaad <firon1222@gmail.com>
2023-04-07 20:05:05 +0200

    update task 4 and 2

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,7 +7,7 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (!n || index >= sizeof(unsigned long int) * 8)
+	if (!n || index >= sizeof(long int) * 8)
 		return (-1);
 	*n &= ~(1UL << index);
 	return (1);
