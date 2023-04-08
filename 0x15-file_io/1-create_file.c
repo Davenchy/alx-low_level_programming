1c5e47ce388c95b522761975fd5a398acd69eadb
Fadi Asaad <firon1222@gmail.com>
2023-04-08 11:14:12 +0200

    update task 1 to fix the created file permissions

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 	 * if file is not exists then
 	 * creates it with permissions 600
 	 */
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd < 0)
 		goto end;
 
