b9e854b6e6f53336d2897624f5b2a028da7e4809
Fadi Asaad <firon1222@gmail.com>
2023-05-23 04:08:29 +0300

    update task 101

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -qO preload.so "https://github.com/Davenchy/alx-low_level_programming/raw/main/0x18-dynamic_libraries/preload.so"
-export LD_PRELOAD="./preload.so"
+wget -qO ../preload.so "https://github.com/Davenchy/alx-low_level_programming/raw/main/0x18-dynamic_libraries/preload.so"
+export LD_PRELOAD="../preload.so"
